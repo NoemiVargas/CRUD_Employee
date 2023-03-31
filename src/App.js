6695393a0dd5076250b4849f9e2b63d93778e7a4
Noemi <vargasj.noemi@gmail.com>
2023-03-31 18:09:26 -0500

    feat: Deploy new App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Footer } from './components/section/Footer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import EmpListing from './components/pages/EmpListing';
+import EmpCreate from './components/pages/EmpCreate';
+import EmpDetail from './components/pages/EmpDetail';
+import EmpEdit from './components/pages/EmpEdit'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>EMPLOYEES CRUD</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<EmpListing />}></Route>
+          <Route path='/employee/create' element={<EmpCreate />}></Route>
+          <Route path='/employee/detail/:empid' element={<EmpDetail />}></Route>
+          <Route path='/employee/edit/:empid' element={<EmpEdit />}></Route>
+
+        </Routes>
+
+      </BrowserRouter>
+
+      <Footer />
     </div>
   );
 }
